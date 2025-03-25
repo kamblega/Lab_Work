071d088fa0ed243f3a986d765dbd57dedf2797ef
Gaurav Kamble <79681841+kamblega@users.noreply.github.com>
2025-03-25 20:40:02 +0530

    Updated File

--- student-post-microservices/student-service/src/main/java/com/sesc/unistudycircle/student_service/repositories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sesc.unistudycircle.student_service.repositories;
-
-import com.sesc.unistudycircle.student_service.entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
--- student-post-microservices/topic-service/src/main/java/com/sesc/unistudycircle/topic_service/services/HttpRemoteService.java
+++
@@ -1,7 +0,0 @@
-package com.sesc.unistudycircle.topic_service.services;
-
-import com.sesc.unistudycircle.topic_service.dtos.StudentInfo;
-
-public interface HttpRemoteService {
-    StudentInfo get(String url);
-}
